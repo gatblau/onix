7b28ba7485350639570ccb46e9cac7d5f8c4215c
gatblau <andres.alos.m@gmail.com>
2018-02-28 22:22:38 +0000

    Initial commit.

--- container/db/image_conf/postgresql-init/init.sh
+++ container/db/image_conf/postgresql-init/init.sh
@@ -2,11 +2,11 @@
 # gets the folder this script is in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo '>>> Creating the ONIX database <<<'
-createdb onix
-
 echo '>>> Creating the ONIX user <<<'
 psql -U postgres -c "CREATE USER onix WITH PASSWORD 'onix';"
+
+echo '>>> Creating the ONIX database <<<'
+createdb -E UTF8 -e -O onix onix
 
 echo '>>> Creating the ONIX tables <<<'
 psql -U postgres -d onix -a -f $DIR/create_tables.sql
