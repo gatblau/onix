c45fa9f69a391db92ff43b21f4c5f3308a305620
gatblau <onix@gatblau.org>
2018-02-28 20:28:33 +0000

    Initial commit.

--- db/build.sh
+++ db/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# builds an onix postgresql database image using the S2I tool
+# builds an onix postgresql database image using the S2I tool (https://github.com/openshift/source-to-image/releases)
 ./s2i build ./image_conf/ centos/postgresql-96-centos7 onix-db:0.0.1-0
