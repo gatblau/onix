22aed4996e4066633731c02475522ee5f781065e
gatblau <onix@gatblau.org>
2018-04-22 08:30:00 +0100

    adds docker compose file

--- install/container/svc/build.sh
+++ install/container/svc/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-s2i build https://github.com/gatblau/onix.git fabric8/s2i-java onix-svc:0.0.1-0
+../s2i build https://github.com/gatblau/onix.git fabric8/s2i-java onix-svc:0.0.1-0
 
