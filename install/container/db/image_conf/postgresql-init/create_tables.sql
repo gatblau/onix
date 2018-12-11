d8ed6b9fbbc4b8ddb062596ef42cb44e19bcae93
gatblau <andres.alos.m@gmail.com>
2018-12-11 20:32:52 +0000

    add hstore validation (required, allowed, none) for items and links. add link types.

--- install/container/db/image_conf/postgresql-init/create_tables.sql
+++ install/container/db/image_conf/postgresql-init/create_tables.sql
@@ -37,7 +37,7 @@ BEGIN
             name CHARACTER VARYING(200) COLLATE pg_catalog."default",
             description CHARACTER VARYING(500) COLLATE pg_catalog."default",
             attr_valid HSTORE,
-            custom boolean DEFAULT TRUE,
+            system boolean DEFAULT FALSE,
             version bigint NOT NULL DEFAULT 1,
             created timestamp(6) with time zone DEFAULT CURRENT_TIMESTAMP(6),
             updated timestamp(6) with time zone,
@@ -54,10 +54,10 @@ BEGIN
         ALTER TABLE item_type
             OWNER to onix;
 
-        INSERT INTO item_type(key, name, description, custom, changedby) VALUES ('INVENTORY', 'Ansible Inventory', 'An Ansible inventory.', FALSE, 'system');
-        INSERT INTO item_type(key, name, description, custom, changedby) VALUES ('HOST-GROUP', 'Host Group', 'An Ansible host group.', FALSE, 'system');
-        INSERT INTO item_type(key, name, description, custom, changedby) VALUES ('HOST', 'Host', 'An Operating System Host.', FALSE, 'system');
-        INSERT INTO item_type(key, name, description, custom, changedby) VALUES ('LICENCE', 'A software licence.', 'Describes the information pertaining to a software licence.', FALSE, 'system');
+        INSERT INTO item_type(key, name, description, system, changedby) VALUES ('INVENTORY', 'Ansible Inventory', 'An Ansible inventory.', TRUE, 'onix');
+        INSERT INTO item_type(key, name, description, system, changedby) VALUES ('HOST-GROUP', 'Host Group', 'An Ansible host group.', TRUE, 'onix');
+        INSERT INTO item_type(key, name, description, system, changedby) VALUES ('HOST', 'Host', 'An Operating System Host.', TRUE, 'onix');
+        INSERT INTO item_type(key, name, description, system, changedby) VALUES ('LICENCE', 'A software licence.', 'Describes the information pertaining to a software licence.', TRUE, 'onix');
 
 	  END IF;
 
@@ -127,6 +127,44 @@ BEGIN
 	  END IF;
 
     ---------------------------------------------------------------------------
+    -- LINK TYPE
+    ---------------------------------------------------------------------------
+    IF NOT EXISTS (SELECT relname FROM pg_class WHERE relname='link_type')
+    THEN
+        CREATE SEQUENCE link_type_id_seq
+        INCREMENT 1
+        START 1
+        MINVALUE 1
+        MAXVALUE 9223372036854775807
+        CACHE 1;
+
+        ALTER SEQUENCE link_type_id_seq
+        OWNER TO onix;
+
+        CREATE TABLE link_type (
+           id INTEGER NOT NULL DEFAULT nextval('link_type_id_seq'::regclass),
+           key CHARACTER VARYING(100) NOT NULL COLLATE pg_catalog."default",
+           name CHARACTER VARYING(200) COLLATE pg_catalog."default",
+           description CHARACTER VARYING(500) COLLATE pg_catalog."default",
+           attr_valid HSTORE,
+           system boolean DEFAULT FALSE,
+           version bigint NOT NULL DEFAULT 1,
+           created timestamp(6) with time zone DEFAULT CURRENT_TIMESTAMP(6),
+           updated timestamp(6) with time zone,
+           changedby CHARACTER VARYING(50) NOT NULL COLLATE pg_catalog."default",
+           CONSTRAINT link_type_id_pk PRIMARY KEY (id),
+           CONSTRAINT link_type_key_uc UNIQUE (key),
+           CONSTRAINT link_type_name_uc UNIQUE (name)
+        )
+        WITH (OIDS = FALSE) TABLESPACE pg_default;
+
+        ALTER TABLE link_type OWNER to onix;
+
+        INSERT INTO link_type(key, name, description, system, changedby) VALUES ('INVENTORY', 'Inventory Link Type.', 'Links items describing an inventory.', TRUE, 'onix');
+        INSERT INTO link_type(key, name, description, system, changedby) VALUES ('LICENSE', 'Licence Link Type.', 'Links items related by a licence.', TRUE, 'onix');
+    END IF;
+
+    ---------------------------------------------------------------------------
     -- LINK
     ---------------------------------------------------------------------------
     IF NOT EXISTS (SELECT relname FROM pg_class WHERE relname='link')
@@ -145,6 +183,7 @@ BEGIN
         (
             id bigint NOT NULL DEFAULT nextval('link_id_seq'::regclass),
             key CHARACTER VARYING(200) COLLATE pg_catalog."default" NOT NULL,
+            link_type_id integer,
             start_item_id bigint NOT NULL,
             end_item_id bigint NOT NULL,
             description text COLLATE pg_catalog."default",
@@ -157,6 +196,10 @@ BEGIN
             changedby CHARACTER VARYING(100) NOT NULL COLLATE pg_catalog."default",
             CONSTRAINT link_id_pk PRIMARY KEY (id),
             CONSTRAINT link_key_uc UNIQUE (key),
+            CONSTRAINT link_link_type_id_fk FOREIGN KEY (link_type_id)
+                REFERENCES link_type (id) MATCH SIMPLE
+                ON UPDATE NO ACTION
+                ON DELETE NO ACTION,
             CONSTRAINT link_end_item_id_fk FOREIGN KEY (end_item_id)
                 REFERENCES item (id) MATCH SIMPLE
                 ON UPDATE NO ACTION
@@ -174,6 +217,15 @@ BEGIN
         ALTER TABLE link
             OWNER to onix;
 
+        CREATE INDEX fki_link_link_type_id_fk
+            ON link USING btree (link_type_id)
+            TABLESPACE pg_default;
+
+        CREATE INDEX fki_link_start_item_id_fk
+            ON link USING btree
+            (start_item_id)
+            TABLESPACE pg_default;
+
         CREATE INDEX fki_link_end_item_id_fk
             ON link USING btree
             (end_item_id)
@@ -189,10 +241,6 @@ BEGIN
         (attribute)
         TABLESPACE pg_default;
 
-        CREATE INDEX fki_link_start_item_id_fk
-            ON link USING btree
-            (start_item_id)
-            TABLESPACE pg_default;
     END IF;
 
     ---------------------------------------------------------------------------
@@ -254,6 +302,7 @@ BEGIN
             change_date TIMESTAMP NOT NULL,
             id bigint,
             key CHARACTER VARYING(200) COLLATE pg_catalog."default",
+            link_type_id integer,
             start_item_id bigint,
             end_item_id bigint,
             description CHARACTER VARYING(500) COLLATE pg_catalog."default",
@@ -304,7 +353,7 @@ BEGIN
             name CHARACTER VARYING(200) COLLATE pg_catalog."default",
             description CHARACTER VARYING(500) COLLATE pg_catalog."default",
             attr_valid HSTORE,
-            custom boolean,
+            system boolean,
             version bigint,
             created timestamp(6) with time zone,
             updated timestamp(6) with time zone,
@@ -333,6 +382,51 @@ BEGIN
         CREATE TRIGGER item_type_audit
         AFTER INSERT OR UPDATE OR DELETE ON item_type
           FOR EACH ROW EXECUTE PROCEDURE audit_item_type();
+    END IF;
+
+    ---------------------------------------------------------------------------
+    -- ITEM_TYPE AUDIT
+    ---------------------------------------------------------------------------
+    IF NOT EXISTS (SELECT relname FROM pg_class WHERE relname='link_type_audit')
+    THEN
+        CREATE TABLE link_type_audit
+        (
+            operation CHAR(1) NOT NULL,
+            changed TIMESTAMP NOT NULL,
+            id INTEGER,
+            key CHARACTER VARYING(100) COLLATE pg_catalog."default",
+            name CHARACTER VARYING(200) COLLATE pg_catalog."default",
+            description CHARACTER VARYING(500) COLLATE pg_catalog."default",
+            attr_valid HSTORE,
+            system boolean,
+            version bigint,
+            created timestamp(6) with time zone,
+            updated timestamp(6) with time zone,
+            changedby CHARACTER VARYING(50) NOT NULL COLLATE pg_catalog."default"
+        );
+
+        ALTER TABLE link_type_audit
+            OWNER to onix;
+
+        CREATE OR REPLACE FUNCTION audit_link_type() RETURNS TRIGGER AS $link_type_audit$
+        BEGIN
+            IF (TG_OP = 'DELETE') THEN
+                INSERT INTO item_type_audit SELECT 'D', now(), OLD.*;
+                RETURN OLD;
+            ELSIF (TG_OP = 'UPDATE') THEN
+                INSERT INTO item_type_audit SELECT 'U', now(), NEW.*;
+                RETURN NEW;
+            ELSIF (TG_OP = 'INSERT') THEN
+                INSERT INTO item_type_audit SELECT 'I', now(), NEW.*;
+                RETURN NEW;
+            END IF;
+                RETURN NULL; -- result is ignored since this is an AFTER trigger
+        END;
+        $link_type_audit$ LANGUAGE plpgsql;
+
+        CREATE TRIGGER link_type_audit
+            AFTER INSERT OR UPDATE OR DELETE ON link_type
+            FOR EACH ROW EXECUTE PROCEDURE audit_link_type();
     END IF;
 END;
 $$
