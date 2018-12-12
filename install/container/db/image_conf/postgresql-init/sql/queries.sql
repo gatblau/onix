cbee4efa8e48bd8287214a6356a218bf9a7b3b30
gatblau <onix@gatblau.org>
2018-12-12 20:15:32 +0000

    restructure sql files. add queries, delete funcs, etc.

---
+++ install/container/db/image_conf/postgresql-init/sql/queries.sql
@@ -0,0 +1,216 @@
+/*
+    Onix CMDB - Copyright (c) 2018-2019 by www.gatblau.org
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software distributed under
+    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+    either express or implied.
+    See the License for the specific language governing permissions and limitations under the License.
+
+    Contributors to this project, hereby assign copyright in this code to the project,
+    to be licensed under the same terms as the rest of the code.
+*/
+DO
+$$
+BEGIN
+
+/*
+  find_items: find items that comply with the passed-in query parameters
+ */
+CREATE OR REPLACE FUNCTION find_items(
+    tag_param text[], -- zero (null) or more tags
+    attribute_param hstore, -- zero (null) or more key->value pair attributes
+    status_param smallint, -- zero (null) or one status
+    item_type_key_param character varying, -- zero (null) or one item type
+    date_created_from_param timestamp(6) with time zone, -- none (null) or created from date
+    date_created_to_param timestamp(6) with time zone, -- none (null) or created to date
+    date_updated_from_param timestamp(6) with time zone, -- none (null) or updated from date
+    date_updated_to_param timestamp(6) with time zone -- none (null) or updated to date
+  )
+  RETURNS TABLE(
+    id bigint,
+    key character varying,
+    name character varying,
+    description text,
+    status smallint,
+    item_type_id integer,
+    meta jsonb,
+    tag text[],
+    attribute hstore,
+    version bigint,
+    created timestamp(6) with time zone,
+    updated timestamp(6) with time zone,
+    changedby character varying
+  )
+  LANGUAGE 'plpgsql'
+  COST 100
+  STABLE
+AS $BODY$
+DECLARE
+  item_type_id_value smallint;
+
+BEGIN
+  IF NOT (item_type_key_param IS NULL) THEN
+    SELECT it.id INTO item_type_id_value
+    FROM item_type it
+      INNER JOIN item i
+        ON i.item_type_id = it.id
+        AND it.key = item_type_key_param;
+  END IF;
+
+  RETURN QUERY SELECT
+    i.id,
+    i.key,
+    i.name,
+    i.description,
+    i.status,
+    i.item_type_id,
+    i.meta,
+    i.tag,
+    i.attribute,
+    i.version,
+    i.created,
+    i.updated,
+    i.changedby
+  FROM item i
+  WHERE
+  -- by item type
+      (i.item_type_id = item_type_id_value OR item_type_id_value IS NULL)
+  -- by status
+  AND (i.status = status_param OR status_param IS NULL)
+  -- by tags
+  AND (i.tag @> tag_param OR tag_param IS NULL)
+  -- by attributes (hstore)
+  AND (i.attribute @> attribute_param OR attribute_param IS NULL)
+  -- by created date range
+  AND ((date_created_from_param <= i.created AND date_created_to_param > i.created) OR
+      (date_created_from_param IS NULL AND date_created_to_param IS NULL) OR
+      (date_created_from_param IS NULL AND date_created_to_param > i.created) OR
+      (date_created_from_param <= i.created AND date_created_to_param IS NULL))
+  -- by updated date range
+  AND ((date_updated_from_param <= i.updated AND date_updated_to_param > i.updated) OR
+      (date_updated_from_param IS NULL AND date_updated_to_param IS NULL) OR
+      (date_updated_from_param IS NULL AND date_updated_to_param > i.updated) OR
+      (date_updated_from_param <= i.updated AND date_updated_to_param IS NULL));
+END
+$BODY$;
+
+ALTER FUNCTION find_items(
+    text[],
+    hstore,
+    smallint,
+    character varying,
+    timestamp(6) with time zone, -- created from
+    timestamp(6) with time zone, -- created to
+    timestamp(6) with time zone, -- updated from
+    timestamp(6) with time zone -- updated to
+  )
+  OWNER TO onix;
+
+/*
+  find_links: find links that comply with the passed-in query parameters
+ */
+CREATE OR REPLACE FUNCTION find_links(
+  start_item_key_param character varying, -- zero (null) or one start item
+  end_item_key_param character varying, -- zero (null) or one end item
+  tag_param text[], -- zero (null) or more tags
+  attribute_param hstore, -- zero (null) or more key->value pair attributes
+  link_type_key_param character varying, -- zero (null) or one link type
+  date_created_from_param timestamp(6) with time zone, -- none (null) or created from date
+  date_created_to_param timestamp(6) with time zone, -- none (null) or created to date
+  date_updated_from_param timestamp(6) with time zone, -- none (null) or updated from date
+  date_updated_to_param timestamp(6) with time zone -- none (null) or updated to date
+)
+RETURNS TABLE(
+    id bigint,
+    key character varying,
+    link_type_key character varying,
+    start_item_key character varying,
+    end_item_key character varying,
+    description text,
+    meta jsonb,
+    tag text[],
+    attribute hstore,
+    version bigint,
+    created TIMESTAMP(6) WITH TIME ZONE,
+    updated timestamp(6) WITH TIME ZONE,
+    changedby CHARACTER VARYING(100)
+  )
+  LANGUAGE 'plpgsql'
+  COST 100
+  STABLE
+AS $BODY$
+DECLARE
+  link_type_id_value smallint;
+
+BEGIN
+  IF NOT (link_type_key_param IS NULL) THEN
+    SELECT lt.id INTO link_type_id_value
+    FROM link_type lt
+      INNER JOIN link l
+        ON l.link_type_id = lt.id
+        AND lt.key = link_type_key_param;
+  END IF;
+
+  RETURN QUERY SELECT
+    l.id,
+    l.key,
+    lt.key as link_type_key,
+    start_item.key AS start_item_key,
+    end_item.key AS end_item_key,
+    l.description,
+    l.meta,
+    l.tag,
+    l.attribute,
+    l.version,
+    l.created,
+    l.updated,
+    l.changedby
+  FROM link l
+  LEFT JOIN item start_item
+    ON l.start_item_id = start_item.id
+  LEFT JOIN item end_item
+    ON l.end_item_id = end_item.id
+  LEFT JOIN link_type lt
+    ON l.link_type_id = lt.id
+  WHERE
+   -- by link type
+   (l.link_type_id = link_type_id_value OR link_type_id_value IS NULL)
+   -- by start item
+   AND (start_item.key = start_item_key_param OR start_item_key_param IS NULL)
+   -- by end item
+   AND (end_item.key = end_item_key_param OR end_item_key_param IS NULL)
+   -- by tags
+   AND (l.tag @> tag_param OR tag_param IS NULL)
+   -- by attributes (hstore)
+   AND (l.attribute @> attribute_param OR attribute_param IS NULL)
+   -- by created date range
+   AND ((date_created_from_param <= l.created AND date_created_to_param > l.created) OR
+        (date_created_from_param IS NULL AND date_created_to_param IS NULL) OR
+        (date_created_from_param IS NULL AND date_created_to_param > i.created) OR
+        (date_created_from_param <= l.created AND date_created_to_param IS NULL))
+   -- by updated date range
+   AND ((date_updated_from_param <= l.updated AND date_updated_to_param > l.updated) OR
+        (date_updated_from_param IS NULL AND date_updated_to_param IS NULL) OR
+        (date_updated_from_param IS NULL AND date_updated_to_param > i.updated) OR
+        (date_updated_from_param <= l.updated AND date_updated_to_param IS NULL));
+END
+$BODY$;
+
+ALTER FUNCTION find_links(
+  character varying,
+  character varying,
+  text[],
+  hstore,
+  character varying,
+  timestamp(6) with time zone, -- created from
+  timestamp(6) with time zone, -- created to
+  timestamp(6) with time zone, -- updated from
+  timestamp(6) with time zone -- updated to
+)
+OWNER TO onix;
+
+END
+$$;
