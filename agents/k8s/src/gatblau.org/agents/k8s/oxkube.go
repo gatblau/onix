fa6a0530f721173ead9367a1d5489bb6f1c82ca4
gatblau <onix@gatblau.org>
2019-06-01 03:48:00 +0100

    initial drop for oxkube

---
+++ agents/k8s/src/gatblau.org/agents/k8s/oxkube.go
@@ -0,0 +1,109 @@
+/*
+   Onix CMDB - Copyright (c) 2018-2019 by www.gatblau.org
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software distributed under
+   the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+   either express or implied.
+   See the License for the specific language governing permissions and limitations under the License.
+
+   Contributors to this project, hereby assign copyright in this code to the project,
+   to be licensed under the same terms as the rest of the code.
+*/
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+/*
+	oxkube is a simple http server process which exposes a webhook
+ */
+func main() {
+	// determines the operation mode i.e. webhook or msg broker
+	mode := get_mode()
+
+	switch mode {
+	case "webhook":
+		run_webhook()
+	case "broker":
+		panic("Mode 'broker' is not implemented.")
+	default:
+		panic(fmt.Sprintf("Mode '%s' is not implemented.", get_mode()))
+	}
+}
+
+// launch a webhook on a TCP port listening for events
+func run_webhook() {
+	// creates an http server listening on the specified TCP port
+	server := &http.Server{Addr: fmt.Sprintf(":%s", get_port()), Handler: nil}
+
+	// registers a handler for the /webhook endpoint
+	http.HandleFunc("/webhook", webhook)
+
+	// runs the server asynchronously
+	go func() {
+		log.Println(fmt.Sprintf("oxkube listening on :%s/webhook", get_port()))
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// creates a channel to pass a SIGINT (ctrl+C) kernel signal with buffer capacity 1
+	stop := make(chan os.Signal, 1)
+
+	// sends any SIGINT signal to the stop channel
+	signal.Notify(stop, os.Interrupt)
+
+	// waits for the SIGINT signal to be raised (pkill -2)
+	<-stop
+
+	// gets a context with some delay to shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	// releases resources if main completes before the delay period elapses
+	defer cancel()
+
+	// on error shutdown
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+		log.Println("shutting down oxkube")
+	}
+}
+
+func get_port() string {
+	return get_env("OX_KUBE_PORT", "8080")
+}
+
+func get_mode() string {
+	return get_env("OX_KUBE_MODE", "webhook")
+}
+
+func get_env(key string, defaultValue string) string {
+	value := ""
+	if os.Getenv(key) != "" {
+		value = os.Getenv(key)
+	} else {
+		value = defaultValue
+	}
+	return value
+}
+
+func webhook(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	switch r.Method {
+	case "GET":
+		io.WriteString(w, "OxKube is ready to process events posted to this endpoint")
+	case "POST":
+		
+	}
+}
