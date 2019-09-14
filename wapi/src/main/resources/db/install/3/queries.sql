ac4dfb2f0ddb5927d1cedfec8caa7073a0f39ff7
gatblau <onix@gatblau.org>
2019-09-14 18:58:45 +0100

    new function to retrieve children items

--- wapi/src/main/resources/db/install/3/queries.sql
+++ wapi/src/main/resources/db/install/3/queries.sql
@@ -899,5 +899,71 @@ $BODY$;
 
 ALTER FUNCTION ox_get_model_link_rules(character varying) OWNER TO onix;
 
+/*
+    ox_get_item_children: get all child items of the specified item.
+ */
+CREATE OR REPLACE FUNCTION ox_get_item_children(
+  item_id bigint,
+  role_key_param character varying[]
+)
+  RETURNS TABLE(
+     id bigint,
+     key character varying,
+     name character varying,
+     description text,
+     status smallint,
+     item_type_key character varying,
+     meta jsonb,
+     tag text[],
+     attribute hstore,
+     version bigint,
+     created timestamp(6) with time zone,
+     updated timestamp(6) with time zone,
+     changed_by character varying,
+     model_key character varying,
+     partition_key character varying
+)
+  LANGUAGE 'plpgsql'
+  COST 100
+  STABLE
+AS $BODY$
+BEGIN
+  RETURN QUERY SELECT
+    i.id,
+    i.key,
+    i.name,
+    i.description,
+    i.status,
+    it.key as item_type_key,
+    i.meta,
+    i.tag,
+    i.attribute,
+    i.version,
+    i.created,
+    i.updated,
+    i.changed_by,
+    m.key as model_key,
+    p.key as partition_key
+    FROM item i
+      INNER JOIN item_type it ON i.item_type_id = it.id
+      INNER JOIN model m ON m.id = it.model_id
+      INNER JOIN partition p on i.partition_id = p.id
+      INNER JOIN privilege pr on p.id = pr.partition_id
+      INNER JOIN role r on pr.role_id = r.id
+      WHERE i.id IN (
+        SELECT unnest((
+            SELECT *
+            FROM ox_get_child_items(item_id)
+          )::bigint[]
+        )
+      )
+      AND pr.can_read = TRUE
+      AND r.key = ANY(role_key_param);
+END;
+$BODY$;
+
+ALTER FUNCTION ox_get_item_children(bigint, character varying[])
+  OWNER TO onix;
+
 END
 $$;
