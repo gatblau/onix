67102ded3ee91bea47fa9d97ac60da51236a3889
gatblau <onix@gatblau.org>
2019-09-20 17:09:05 +0100

    display metadata

--- wc/pages/dashboard/item/_itemKey.vue
+++ wc/pages/dashboard/item/_itemKey.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="table-wrapper-scroll-y my-custom-scrollbar">
-        <button type="button" class="btn btn-outline-primary" onclick="window.history.back()"><i class="material-icons">keyboard_arrow_up</i></button>
+        <table>
+            <td><button type="button" class="btn btn-outline-primary" onclick="window.history.back()"><i class="material-icons">keyboard_arrow_up</i></button></td>
+            <td><h3>Children of <i>{{title}}</i></h3></td>
+        </table>
         <table class="table table-bordered table-striped table-hover mb-0">
             <thead class="thead-light">
             <tr>
@@ -21,13 +24,25 @@
                     <button
                             type="button"
                             class="btn btn-outline-primary"
-                            v-on:click="onItemClick"
+                            v-on:click="onItemIn"
+                            :value="item.key"
+                    >in</button>
+                    <button
+                            type="button"
+                            class="btn btn-outline-primary"
+                            v-on:click="onItemView"
                             :value="item.key"
                     >view</button>
                 </td>
             </tr>
             </tbody>
         </table>
+        <div v-if="showMeta">
+            <h4>Metadata</h4>
+            <div class="overflow-auto" style="max-height: 250px;">
+                <code>{{meta}}</code>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -35,17 +50,30 @@
     export default {
         name: "itemKey",
         methods: {
-            onItemClick(data){
+            onItemIn(data) {
                 this.$router.push('../item/' + data.target.value);
+            },
+            onItemView(data) {
+                this.$store.commit('graph/setMeta', { itemKey: data.target.value, app: this });
             }
         },
         computed: {
-            items () {
+            items() {
                 return this.$store.state.graph.items;
+            },
+            meta() {
+                return JSON.stringify(this.$store.state.graph.meta);
+            },
+            showMeta() {
+                return this.$store.state.graph.meta != null;
+            },
+            title(){
+                return this.$route.params.itemKey;
             }
         },
-        // watchQuery: true,
         async asyncData ({ params, $axios, app, store }) {
+            store.commit('graph/setMeta', { itemKey: "", app: this });
+            store.commit('graph/setTitle', { title: params.itemKey, app:this });
             $axios.get('/api/item/' + params.itemKey + "/children")
                 .then((result) => {
                     store.commit('graph/setItems', { items: result.data.values, app: app });
