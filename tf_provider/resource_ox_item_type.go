fe74b286e4b75942b03c73ea8bc16434d7f286ce
gatblau <onix@gatblau.org>
2020-03-25 18:58:25 +0000

    update docs

--- tf_provider/resource_ox_item_type.go
+++ tf_provider/resource_ox_item_type.go
@@ -77,18 +77,6 @@ func ItemTypeResource() *schema.Resource {
 				Type:     schema.TypeInt,
 				Optional: true,
 			},
-			"created": &schema.Schema{
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"updated": &schema.Schema{
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"changed_by": &schema.Schema{
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
 		},
 	}
 }
