f6097d15b4ea3a2b45569784071ce0f17b949f2a
gatblau <onix@gatblau.org>
2020-04-05 10:01:09 +0100

    build scripts for container image

--- tf_backend/main.go
+++ tf_backend/main.go
@@ -1,5 +1,6 @@
 /*
-   Terraform Http Backend - Onix - Copyright (c) 2018 by www.gatblau.org
+   Onix Config Manager - OxTerra - Terraform Http Backend for Onix
+   Copyright (c) 2018-2020 by www.gatblau.org
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
