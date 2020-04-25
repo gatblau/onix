eb07a7917754d17f13cb75372947f8240b346249
gatblau <onix@gatblau.org>
2020-04-25 20:26:09 +0100

    add docusaurus documentation site

---
+++ man/docs/started_config.md
@@ -0,0 +1,4 @@
+---
+id: started_config
+title: Configuration
+---
---
+++ man/sidebars.js
@@ -0,0 +1,16 @@
+module.exports = {
+  someSidebar: {
+    'Onix': [
+      {type:'doc', id:'onix_intro'}
+    ],
+    'Getting Started': [
+      {type:'doc', id:'started_install'},
+      {type:'doc', id:'started_config'}
+    ],
+    'Concepts': [
+      {type:'doc', id:'doc1'}
+    ],
+    'Architecture': [],
+    'Features': [],
+  },
+};
