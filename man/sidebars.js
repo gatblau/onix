083e790c41b861b34c3735b62fcb5fca0e99df20
gatblau <onix@gatblau.org>
2020-05-10 09:57:03 +0100

    docs: new concepts -> database

--- man/sidebars.js
+++ man/sidebars.js
@@ -10,7 +10,7 @@ module.exports = {
     'Concepts': [
       {type:'doc', id:'concept_users'},
       {type:'doc', id:'concept_roles'},
-      {type:'doc', id:'concept_items'},
+      {type:'doc', id:'concept_db'},
     ],
     'Architecture': [],
     'Features': [],
