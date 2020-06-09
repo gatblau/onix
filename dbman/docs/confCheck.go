2b2fe48a3f4195f6341d08e9c88a4b2fcf8fd537
gatblau <onix@gatblau.org>
2020-06-09 14:19:02 +0100

    improve documentation

---
+++ dbman/docs/confCheck.go
@@ -0,0 +1,19 @@
+package docs
+
+// swagger:route GET /conf/check configuration check
+//
+// check
+//
+// Checks that a connection can be established to the database and remote git repository based on the specified configuration.
+//
+// Configuration is provided as environment variables to the running service.
+//
+// responses:
+//        200: confCheckResponse
+
+// A list of checks and their result displayed in text form.
+// swagger:response confCheckResponse
+type confCheckResponse struct {
+	// in:body
+	Body string
+}
