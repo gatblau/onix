c6b5ccf697869876f742be3173df48027994c6e2
Steve Cliff <steve@devcloud.guru>
2020-09-05 12:27:52 +0100

    Added ContentType awareness for reload (#20)

--- pilot/core/config.go
+++ pilot/core/config.go
@@ -32,9 +32,10 @@ type Config struct {
 	// a command to reload the app configuration
 	ReloadCmd string
 	// a URI of the endpoint to call to reload the configuration
-	ReloadURI     string
-	ReloadURIUser string
-	ReloadURIPwd  string
+	ReloadURI            string
+	ReloadURIUser        string
+	ReloadURIPwd         string
+        ReloadURIContentType string
 	// a URI to check the application is ready after reloading the configuration
 	ReadyURI string
 	// the viper instance
@@ -90,6 +91,7 @@ func NewConfig() (*Config, error) {
 	_ = v.BindEnv("App.ReloadURI")
 	_ = v.BindEnv("App.ReloadURIUser")
 	_ = v.BindEnv("App.ReloadURIPwd")
+	_ = v.BindEnv("App.ReloadURIContentType")
 	_ = v.BindEnv("App.ReadyURI")
 
 	// creates a config struct and populate it with values
@@ -137,6 +139,7 @@ func NewConfig() (*Config, error) {
 	c.ReloadURI = v.GetString("App.ReloadURI")
 	c.ReloadURIUser = v.GetString("App.ReloadURIUser")
 	c.ReloadURIPwd = v.GetString("App.ReloadURIPwd")
+	c.ReloadURIContentType = v.GetString("App.ReloadURIContentType")
 	c.ReadyURI = v.GetString("App.ReadyURI")
 	return c, nil
 }
