c9145b8470d602013bdd5b274bd45c7b2c1d6403
gatblau <onix@gatblau.org>
2020-09-30 22:36:14 +0100

    client and server communicate with 3 type of messages (logs, vars, files)

--- protoapp/static/js/script.js
+++ protoapp/static/js/script.js
@@ -1,49 +1,56 @@
+function write(parent, message) {
+    let item = document.createElement("div");
+    for (let i = 0; i < message.body.length; i++) {
+        let item = document.createElement("div");
+        item.innerText = message.body[i];
+        let doScroll = parent.scrollTop > parent.scrollHeight - parent.clientHeight - 1;
+        parent.appendChild(item);
+        if (doScroll) {
+            parent.scrollTop = parent.scrollHeight - parent.clientHeight;
+        }
+    }
+}
+
 window.onload = function () {
     let conn;
     let log = document.getElementById("log");
+    let cfgfile = document.getElementById("cfgfile");
+    let vars = document.getElementById("env");
     let clear = document.getElementById("clear")
 
-    clear.onclick = function() {
-        log.innerHTML = ""
-    }
-
-    function appendLog(item) {
-        let doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
-        log.appendChild(item);
-        if (doScroll) {
-            log.scrollTop = log.scrollHeight - log.clientHeight;
-        }
-    }
+    // clear terminal window
+    clear.onclick = function() { log.innerHTML = "" }
 
     if (window["WebSocket"]) {
         // creates a new WebSocket connection
         conn = new WebSocket("ws://" + document.location.host + "/ws");
         conn.onopen = function(event) {
-            let item = document.createElement("div");
-            item.innerHTML = "<b>Connection established.</b>";
-            appendLog(item);
+            write(log, { type:2, body: ["Connection established."]});
         }
         conn.onclose = function (evt) {
-            let item = document.createElement("div");
-            item.innerHTML = "<b>Connection closed.</b>";
-            appendLog(item);
+            write(log, { type:2, body: ["Connection closed."]});
         };
         conn.onmessage = function (evt) {
-            let messages = evt.data.split('\n');
-            for (let i = 0; i < messages.length; i++) {
-                let item = document.createElement("div");
-                item.innerText = messages[i];
-                appendLog(item);
+            // parse the message string into a JSON object
+            let message = JSON.parse(evt.data);
+
+            // the message is an event
+            if (message.type == 0) {
+                write(log, message)
+            }
+            // the message contain one or more configuration files
+            if (message.type == 1) {
+                write(cfgfile, message)
+            }
+            // the message has environment variables
+            if (message.type == 2) {
+                write(vars, message)
             }
         };
         conn.onerror = function (evt) {
-            let item = document.createElement("div");
-            item.innerHTML = "<b>Connection error.</b>";
-            appendLog(item);
+            write(log, { type:2, body: ["Connection error."]});
         };
     } else {
-        let item = document.createElement("div");
-        item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
-        appendLog(item);
+        write(log, { type:2, body: ["Your browser does not support WebSockets."]});
     }
 };
