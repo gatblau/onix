029dd34fec885778a1ef4b6e5d6542ad040601c7
gatblau <onix@gatblau.org>
2020-10-18 10:51:47 +0100

    add URI to front matter

--- pilot/core/fm.go
+++ pilot/core/fm.go
@@ -12,32 +12,62 @@ package core
 import (
 	"errors"
 	"fmt"
-	"strings"
+	"net/url"
 )
 
-// pilot configuration stored in Onix along with the application configuration
+// pilot configuration for a specific application configuration
+// informs pilot how to manage the application configuration
 type frontMatter struct {
-	Type        string `json:"type"`
-	Path        string `json:"path"`
-	User        string `json:"user"`
-	Pwd         string `json:"pwd"`
-	Trigger     string `json:"trigger"`
+	// the type of configuration to handle (i.e. file, http or environment)
+	Type string `json:"type"`
+	// the file path to the configuration (only applicable if Type=file)
+	Path string `json:"path"`
+	// the URI used to issue http requests to manage the configuration (only applicable if Type is not file)
+	Uri string `json:"uri"`
+	// the Content-Type http header used for POST/PUT requests to the URI
 	ContentType string `json:"contentType"`
+	// the username to authenticate with the URI (if required)
+	User string `json:"user"`
+	// the password to authenticate with the URI (if required)
+	Pwd string `json:"pwd"`
+	// the mechanism used to inform the application to reload the configuration (signal, get, post, put or reload)
+	Trigger string `json:"trigger"`
 }
 
 // validate the front matter
 func (fm *frontMatter) valid() (bool, error) {
-	// check for required fields
-	if ok, err := fm.notEmpty("type", fm.Type); !ok {
+	// a configuration type is always required
+	if ok, err := fm.exists("type", fm.Type); !ok {
 		return ok, err
 	}
-	if ok, err := fm.notEmpty("path", fm.Type); !ok {
+	// does it contain an implemented configuration type?
+	if ok, err := fm.validConfType(fm.Type); !ok {
 		return ok, err
 	}
-	if ok, err := fm.notEmpty("trigger", fm.Trigger); !ok {
+	// a path is only required if the configuration type is a file
+	if ok, err := fm.existsWhen("path", fm.Path, fm.Type == TypeFile.String()); !ok {
 		return ok, err
 	}
-	// default value for content type
+	// a URI is only required if the configuration type is not a file
+	if ok, err := fm.existsWhen("uri", fm.Uri, fm.Type != TypeFile.String()); !ok {
+		return ok, err
+	}
+	// does it contain an valid URI?
+	if fm.Type != TypeFile.String() {
+		_, err := url.ParseRequestURI(fm.Uri)
+		if err != nil {
+			return false, err
+		}
+	}
+	// the trigger should always be provided
+	if ok, err := fm.exists("trigger", fm.Trigger); !ok {
+		return ok, err
+	}
+	// does it contain an implemented trigger?
+	if ok, err := fm.validTrigger(fm.Trigger); !ok {
+		return ok, err
+	}
+	// set a default value for the content type of not provided
 	if len(fm.ContentType) == 0 {
 		fm.ContentType = "text/plain"
 	}
@@ -45,10 +75,7 @@ func (fm *frontMatter) valid() (bool, error) {
 	if ok, err := fm.validContentType(fm.ContentType); !ok {
 		return ok, err
 	}
-	// validates trigger
-	if ok, err := fm.validTrigger(fm.Trigger); !ok {
-		return ok, err
-	}
+
 	return true, nil
 }
 
@@ -62,10 +89,21 @@ func (fm *frontMatter) triggerVal() trigger {
 	return t.parse(fm.Trigger)
 }
 
-// check a field is not empty
-func (fm *frontMatter) notEmpty(field string, value string) (bool, error) {
+// check if a field exists
+func (fm *frontMatter) exists(field string, value string) (bool, error) {
 	if len(value) == 0 {
 		return false, errors.New(fmt.Sprintf("the front matter is missing value for '%s'", field))
+	}
+	return true, nil
+}
+
+// check if a field exists only if the passed in condition is true
+func (fm *frontMatter) existsWhen(field string, value string, condition bool) (bool, error) {
+	if condition {
+		if len(value) == 0 {
+			return false, errors.New(fmt.Sprintf("the front matter is missing value for '%s'", field))
+		}
+		return true, nil
 	}
 	return true, nil
 }
@@ -88,27 +126,22 @@ func (fm *frontMatter) validContentType(contentType string) (bool, error) {
 	}
 }
 
-// check validity of trigger
-func (fm *frontMatter) validTrigger(trigger string) (bool, error) {
-	switch strings.ToLower(trigger) {
-	case "":
-		fallthrough
-	case "restart":
-		fallthrough
-	case "get":
-		fallthrough
-	case "post":
-		fallthrough
-	case "put":
-		return true, nil
-	default:
-		if strings.HasPrefix(strings.ToLower(trigger), "signal:") {
-			s := trigger[7:]
-			if !(s == "SIGHUP" || s == "SIGUSR1" || s == "SIGUSR2") {
-				return false, errors.New(fmt.Sprintf("invalid signal if front matter: %s", s))
-			}
-			return true, nil
-		}
+// check if the provided configuration type matches one of the valid values
+func (fm *frontMatter) validConfType(cType string) (bool, error) {
+	t := new(confType)
+	valid := t.parse(cType) != TypeUnknown
+	if !valid {
+		return false, errors.New(fmt.Sprintf("invalid configuration type %s", cType))
 	}
-	return false, errors.New(fmt.Sprintf("invalid trigger if front matter: %s", trigger))
+	return valid, nil
+}
+
+// check if the provided reload trigger matches one of the valid values
+func (fm *frontMatter) validTrigger(trig string) (bool, error) {
+	t := new(trigger)
+	valid := t.parse(trig) != TriggerUnknown
+	if !valid {
+		return false, errors.New(fmt.Sprintf("invalid reload trigger type %s", trig))
+	}
+	return valid, nil
 }
