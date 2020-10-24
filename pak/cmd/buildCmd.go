e318e734df150e45170878d24cd66a50d266a7da
gatblau <onix@gatblau.org>
2020-10-24 15:03:15 +0100

    build both from a remote repo and from the file system

--- pak/cmd/buildCmd.go
+++ pak/cmd/buildCmd.go
@@ -8,19 +8,20 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/gatblau/onix/pak/core"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 // create a file seal
 type BuildCmd struct {
-	cmd      *cobra.Command
-	branch   string
-	tag      string
-	packer   *core.Builder
-	gitToken string
+	cmd         *cobra.Command
+	branch      string
+	gitTag      string
+	packageName string
+	builder     *core.Builder
+	gitToken    string
+	from        string
+	fromPath    string
 }
 
 func NewBuildCmd() *BuildCmd {
@@ -30,24 +31,18 @@ func NewBuildCmd() *BuildCmd {
 			Short: "build a package",
 			Long:  ``,
 		},
-		packer: core.NewBuilder(),
+		builder: core.NewBuilder(),
 	}
 	c.cmd.Run = c.Run
 	c.cmd.Flags().StringVarP(&c.branch, "branch", "b", "", "the git branch to use")
-	c.cmd.Flags().StringVarP(&c.tag, "tag", "t", "", "the git tag to use")
+	c.cmd.Flags().StringVarP(&c.gitTag, "gitTag", "l", "", "the git tag to use")
 	c.cmd.Flags().StringVarP(&c.gitToken, "token", "k", "", "the git access token")
+	c.cmd.Flags().StringVarP(&c.packageName, "packageName", "t", "", "name and optionally a tag in the 'name:tag' format")
+	c.cmd.Flags().StringVarP(&c.gitToken, "path", "p", "", "the path within the git repository where the root of the source to package is")
 	return c
 }
 
-func (c *BuildCmd) Run(cmd *cobra.Command, args []string) {
-	// get the url of the remote git repository containing the source code
-	gitRepoUrl := args[0]
-	// execute the build
-	c.packer.Build(gitRepoUrl, c.gitToken)
-}
-
-// return the working path
-func path() string {
-	basePath, _ := os.Getwd()
-	return fmt.Sprintf("%s/.pak", basePath)
+func (b *BuildCmd) Run(cmd *cobra.Command, args []string) {
+	b.from = args[0]
+	b.builder.Build(b.from, b.gitToken, b.packageName)
 }
