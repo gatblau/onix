06c951a3ca0597854350e22bbf0f60d6ac84b293
gatblau <onix@gatblau.org>
2020-12-13 14:13:34 +0000

    .

--- builder/server/server.go
+++ builder/server/server.go
@@ -1,5 +1,5 @@
 /*
-  Onix Config Manager - Artie
+  Onix Config Manager - Buildman
   Copyright (c) 2018-2020 by www.gatblau.org
   Licensed under the Apache License, Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0
   Contributors to this project, hereby assign copyright in this code to the project,
