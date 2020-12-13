9ce88a9572b310322fcea77abd61c7fa7f658523
gatblau <onix@gatblau.org>
2020-12-13 14:10:41 +0000

    initial scaffolding

---
+++ builder/server/imageinfo.go
@@ -0,0 +1,17 @@
+package server
+
+// response from skopeo inspect docker://image-name-tag --raw
+type ImgInfo struct {
+	SchemaVersion int    `json:"schemaVersion"`
+	MediaType     string `json:"mediaType"`
+	Config        struct {
+		MediaType string `json:"mediaType"`
+		Size      int    `json:"size"`
+		Digest    string `json:"digest"`
+	} `json:"config"`
+	Layers []struct {
+		MediaType string `json:"mediaType"`
+		Size      int    `json:"size"`
+		Digest    string `json:"digest"`
+	} `json:"layers"`
+}
