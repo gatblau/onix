641ad0e054ee24b4c732820474ff36463af2e396
gatblau <onix@gatblau.org>
2020-12-15 13:28:03 +0000

    update pipe artefact

--- artie/tkn/artefactpipeline_test.go
+++ artie/tkn/artefactpipeline_test.go
@@ -1,7 +1,12 @@
 package tkn
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSurvey(t *testing.T) {
-
+	// collects information to assemble the pipeline
+	c := NewArtPipelineConfig(".", "", false)
+	// assembles the pipeline
+	_ = MergeArtPipe(c, false)
 }
