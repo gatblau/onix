815f88bf704d248eb69322d3e5b57121702f1cd0
gatblau <onix@gatblau.org>
2021-02-15 15:48:11 +0000

    i18n loaded from dictionary files

--- artisan/i18n/lib.go
+++ artisan/i18n/lib.go
@@ -10,49 +10,37 @@ package i18n
 import (
 	"fmt"
 	"github.com/gatblau/onix/artisan/core"
+	"github.com/pelletier/go-toml"
 	"os"
+	"path"
 	"strings"
 )
 
 func String(key I18NKey) string {
-	return msg(key)
+	return get(key)
 }
 
 // prints a localised message
 func Printf(key I18NKey, a ...interface{}) {
-	fmt.Printf(msg(key), a...)
+	fmt.Printf(get(key), a...)
 }
 
 // checks for the  error and if it exists prints a localised error
 func Err(err error, key I18NKey, a ...interface{}) {
 	if err != nil {
-		fmt.Printf("%s - %s\n", fmt.Sprintf(msg(key), a...), err)
+		fmt.Printf("%s - %s\n", fmt.Sprintf(get(key), a...), err)
 		os.Exit(1)
 	}
 }
 
-// return a localised message for the current user
-func msg(key I18NKey) string {
-	// get the key in the user language map
-	value, found := getMap()[key]
-	// if the key is not found or it is empty
-	if !found || len(value) == 0 {
-		// if in debug mode, show if a language specific key is missing
-		core.Debug("key '%d' not found in language '%s'", key, lang())
-		// get the value from the english map
-		value = msg_en[key]
-	}
-	return value
-}
-
-func getMap() map[I18NKey]string {
+func get(key I18NKey) string {
 	var language string
 	// check if internationalisation is enabled
 	inter := os.Getenv("ARTISAN_I18N")
 	// if not then use english
 	if len(inter) == 0 {
 		core.Debug("i18n is disabled, to enable set ARTISAN_I18N")
-		return msg_en
+		return msg_en[key]
 	}
 	// check if an overriding language has been set
 	overrideLang := os.Getenv("ARTISAN_LANG")
@@ -64,22 +52,19 @@ func getMap() map[I18NKey]string {
 		// use the overriding language
 		language = overrideLang
 	}
-	// return the map for the user language
-	switch language {
-	case "es":
-		return msg_es
-	case "de":
-		return msg_de
-	case "hi":
-		return msg_hi
-	case "zh":
-		return msg_zh
-	case "fr":
-		return msg_fr
-	default:
-		// defaults to english
-		return msg_en
+	// load the dictionary from file
+	t, err := toml.LoadFile(path.Join(core.LangPath(), fmt.Sprintf("%s_i18n.toml", language)))
+	var value interface{}
+	if err == nil {
+		value = t.Get(string(key))
+		if value == nil {
+			// set value in english
+			value = msg_en[key]
+		}
+	} else {
+		value = msg_en[key]
 	}
+	return value.(string)
 }
 
 func splitLocale(locale string) (language string, territory string) {
