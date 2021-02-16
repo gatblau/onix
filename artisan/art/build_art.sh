2f8d55dc2fe8535f299822cc58e9d35e78be757a
gatblau <onix@gatblau.org>
2021-02-16 16:54:32 +0000

    .

--- artisan/art/build_art.sh
+++
@@ -1,3 +0,0 @@
-echo $ARTISAN_I18N
-art build -t=artisan-registry-amosonline-aws-01-sapgatewaycd.apps.amosds.amosonline.io/gatblau/artisan
-art push -u=admin:nxrpsap artisan-registry-amosonline-aws-01-sapgatewaycd.apps.amosds.amosonline.io/gatblau/artisan
