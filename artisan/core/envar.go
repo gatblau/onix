f076c82d17067e9ab84c82cd03a0b60d74f8dc29
gatblau <onix@gatblau.org>
2021-03-15 18:01:14 +0000

    add parsing of empty / new lines

--- artisan/core/envar.go
+++ artisan/core/envar.go
@@ -1,3 +1,10 @@
+/*
+  Onix Config Manager - Artisan
+  Copyright (c) 2018-2021 by www.gatblau.org
+  Licensed under the Apache License, Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0
+  Contributors to this project, hereby assign copyright in this code to the project,
+  to be licensed under the same terms as the rest of the code.
+*/
 package core
 
 import (
@@ -27,7 +34,10 @@ func NewEnVarFromFile(envFile string) (*Envar, error) {
 	if err == nil {
 		for _, line := range content {
 			// skips comments
-			if strings.HasPrefix(strings.Trim(line, " "), "#") || len(strings.Trim(line, " ")) == 0 {
+			if strings.HasPrefix(strings.Trim(line, " "), "#") ||
+				len(strings.Trim(line, " ")) == 0 ||
+				strings.HasPrefix(strings.Trim(line, " "), "\r") ||
+				strings.HasPrefix(strings.Trim(line, " "), "\n") {
 				continue
 			}
 			keyValue := strings.Split(line, "=")
