fe69e96d60cf3ce8aa2a95d9c30f98802fc1a058
gatblau <onix@gatblau.org>
2021-04-14 08:30:02 +0100

    project scaffold

---
+++ rem/version.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+TAG=$1
+if [ $# -eq 0 ]; then
+    echo "A tag is required for Onix Artisan. Provide it as a parameter."
+    echo "Usage is: sh version.sh [TAG] - e.g. sh version.sh my-tag"
+    exit 1
+fi
+
+rm ./core/version.go || true
+rm ./version || true
+pwd
+printf "package core\nconst Version=\"%s\"" "${TAG}" > ./core/version.go
+echo "${TAG}" >> version
