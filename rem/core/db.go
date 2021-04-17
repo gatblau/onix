d86aa177bd245c02ff5507c181f200eee08dabc7
gatblau <onix@gatblau.org>
2021-04-17 08:23:02 +0100

    update notice

--- rem/core/db.go
+++ rem/core/db.go
@@ -1,3 +1,10 @@
+/*
+  Onix Config Manager - REMote Host Service
+  Copyright (c) 2018-2021 by www.gatblau.org
+  Licensed under the Apache License, Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0
+  Contributors to this project, hereby assign copyright in this code to the project,
+  to be licensed under the same terms as the rest of the code.
+*/
 package core
 
 import (
@@ -12,14 +19,6 @@ import (
 	"strconv"
 	"time"
 )
-
-/*
-  Onix Config Manager - REMote Host Service
-  Copyright (c) 2018-2021 by www.gatblau.org
-  Licensed under the Apache License, Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0
-  Contributors to this project, hereby assign copyright in this code to the project,
-  to be licensed under the same terms as the rest of the code.
-*/
 
 func NewDb(host, port, db, uname, pwd string) *Db {
 	return &Db{
