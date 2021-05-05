ded075aa2ba985ada4fdc0589a40713feae3e034
gatblau <onix@gatblau.org>
2021-05-05 19:04:05 +0100

    update comments

--- artisan/artreg/backend/backend.go
+++ artisan/artreg/backend/backend.go
@@ -1,3 +1,5 @@
+package backend
+
 /*
   Onix Config Manager - Artisan
   Copyright (c) 2018-2021 by www.gatblau.org
@@ -5,8 +7,6 @@
   Contributors to this project, hereby assign copyright in this code to the project,
   to be licensed under the same terms as the rest of the code.
 */
-package backend
-
 import (
 	"github.com/gatblau/onix/artisan/core"
 	"github.com/gatblau/onix/artisan/registry"
@@ -14,16 +14,18 @@ import (
 	"os"
 )
 
-// the interface implemented by a backend
+// Backend the interface implemented by a backend
 type Backend interface {
-	// upload an package to the remote repository
+	// UploadPackage upload an package to the remote repository
 	UploadPackage(name *core.PackageName, packageRef string, zipfile multipart.File, jsonFile multipart.File, repo multipart.File, user string, pwd string) error
-	// get repository information
+	// GetRepositoryInfo get repository information
 	GetRepositoryInfo(group, name, user, pwd string) (*registry.Repository, error)
-	// get package information
+	// GetPackageInfo get package information
 	GetPackageInfo(group, name, id, user, pwd string) (*registry.Package, error)
-	// update package information
-	UpdatePackageInfo(group string, name string, packageInfo *registry.Package, user string, pwd string) error
-	// open a file for download
+	// UpdatePackageInfo update package information
+	UpdatePackageInfo(name *core.PackageName, packageInfo *registry.Package, user string, pwd string) error
+	// Download open a file for download
 	Download(repoGroup, repoName, fileName, user, pwd string) (*os.File, error)
+	// Name print usage info
+	Name() string
 }
