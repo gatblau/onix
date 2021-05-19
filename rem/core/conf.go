37710c086a6688e3142eae89abe7e850d92e18e4
gatblau <onix@gatblau.org>
2021-05-19 13:43:48 +0100

    add configuration for Onix WAPI

--- rem/core/conf.go
+++ rem/core/conf.go
@@ -16,13 +16,17 @@ import (
 type ConfKey string
 
 const (
-	ConfDbName          ConfKey = "OX_REM_DB_NAME"
-	ConfDbHost          ConfKey = "OX_REM_DB_HOST"
-	ConfDbPort          ConfKey = "OX_REM_DB_PORT"
-	ConfDbUser          ConfKey = "OX_REM_DB_USER"
-	ConfDbPwd           ConfKey = "OX_REM_DB_PWD"
-	ConfRefreshInterval ConfKey = "OX_REM_REFRESH_INTERVAL"
-	ConfPingInterval    ConfKey = "OX_REM_PING_INTERVAL"
+	ConfDbName                   ConfKey = "OX_REM_DB_NAME"
+	ConfDbHost                   ConfKey = "OX_REM_DB_HOST"
+	ConfDbPort                   ConfKey = "OX_REM_DB_PORT"
+	ConfDbUser                   ConfKey = "OX_REM_DB_USER"
+	ConfDbPwd                    ConfKey = "OX_REM_DB_PWD"
+	ConfRefreshInterval          ConfKey = "OX_REM_REFRESH_INTERVAL"
+	ConfPingInterval             ConfKey = "OX_REM_PING_INTERVAL"
+	ConfOxWapiUri                ConfKey = "OX_WAPI_URI"
+	ConfOxWapiUser               ConfKey = "OX_WAPI_USER"
+	ConfOxWapiPwd                ConfKey = "OX_WAPI_PWD"
+	ConfOxWapiInsecureSkipVerify ConfKey = "OX_WAPI_INSECURE_SKIP_VERIFY"
 )
 
 type Conf struct {
@@ -102,4 +106,34 @@ func (c *Conf) GetPingInterval() int {
 		return defaultValue
 	}
 	return v
+}
+
+func (c *Conf) getOxWapiUrl() string {
+	return c.getValue(ConfOxWapiUri)
+}
+
+func (c *Conf) getOxWapiUsername() string {
+	return c.getValue(ConfOxWapiUser)
+}
+
+func (c *Conf) getOxWapiPassword() string {
+	return c.getValue(ConfOxWapiPwd)
+}
+
+func (c *Conf) getValue(key ConfKey) string {
+	value := os.Getenv(string(key))
+	if len(value) == 0 {
+		fmt.Printf("ERROR: variable %s not defined", key)
+		os.Exit(1)
+	}
+	return value
+}
+
+func (c *Conf) getOxWapiInsecureSkipVerify() bool {
+	b, err := strconv.ParseBool(c.getValue(ConfOxWapiInsecureSkipVerify))
+	if err != nil {
+		fmt.Printf("ERROR: invalid value for variable %s", ConfOxWapiInsecureSkipVerify)
+		os.Exit(1)
+	}
+	return b
 }
