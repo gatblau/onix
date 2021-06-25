ef3ac7221f2a5cc0943454b7bb1dc87be596f748
gatblau <onix@gatblau.org>
2021-06-25 13:35:01 +0100

    add generic git clone

---
+++ artisan/core/git.go
@@ -0,0 +1,35 @@
+package core
+
+/*
+  Onix Config Manager - Artisan
+  Copyright (c) 2018-2021 by www.gatblau.org
+  Licensed under the Apache License, Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0
+  Contributors to this project, hereby assign copyright in this code to the project,
+  to be licensed under the same terms as the rest of the code.
+*/
+import (
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"os"
+)
+
+// clones a remote git LocalRegistry, it only accepts a token if authentication is required
+// if the token is not provided (empty string) then no authentication is used
+func gitClone(repoUrl string, gitToken string, sourceDir string) (*git.Repository, error) {
+	// clone the remote repository
+	opts := &git.CloneOptions{
+		URL:      repoUrl,
+		Progress: os.Stdout,
+	}
+	// if authentication token has been provided
+	if len(gitToken) > 0 {
+		// The intended use of a GitHub personal access token is in replace of your password
+		// because access tokens can easily be revoked.
+		// https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
+		opts.Auth = &http.BasicAuth{
+			Username: "abc123", // yes, this can be anything except an empty string
+			Password: gitToken,
+		}
+	}
+	return git.PlainClone(sourceDir, false, opts)
+}
