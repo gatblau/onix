dd0f12e609b1b3e45d70377a2858e15c41d84a06
Steve Cliff <steve.cliff@atos.net>
2021-07-16 18:33:40 +0100

    Alignment to new PGP key usage in Docker

--- pilotctl/boot.sh
+++ pilotctl/boot.sh
@@ -1,5 +1,6 @@
 # fetch user home directory from /etc/passwd file
 export USER_HOME=$(awk -F":" '{print $6}' /etc/passwd | grep -m1 `whoami`)
-mkdir -p ${USER_HOME}/keys
-cp -R /keys ${USER_HOME}
+#mkdir -p ${USER_HOME}/keys
+#cp -R /keys ${USER_HOME}
+cp /keys/.pilot_sign.pgp /app/.
 /app/pilotctl
