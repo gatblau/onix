a187215ee80c075d748ef7d210ca09791ac6bc89
gatblau <onix@gatblau.org>
2021-08-05 15:16:34 +0100

    creates a markdown document file from a package manifest

--- artisan/art/cmd/manifCmd.go
+++ artisan/art/cmd/manifCmd.go
@@ -15,12 +15,16 @@ import (
 	"github.com/gatblau/onix/artisan/registry"
 	"github.com/spf13/cobra"
 	"github.com/yalp/jsonpath"
+	"io/ioutil"
+	"os"
+	"path"
 )
 
-// return package's manifest
+// ManifestCmd return package's manifest
 type ManifestCmd struct {
 	cmd    *cobra.Command
 	filter string
+	format string
 }
 
 func NewManifestCmd() *ManifestCmd {
@@ -32,6 +36,8 @@ func NewManifestCmd() *ManifestCmd {
 		},
 	}
 	c.cmd.Flags().StringVarP(&c.filter, "filter", "f", "", "--filter=JSONPath or -f=JSONPath")
+	c.cmd.Flags().StringVarP(&c.format, "format", "o", "json", "--format=md or -f=md\n"+
+		"available formats are 'json' (in std output) or 'mdf' (creates a markdown file)\n")
 	c.cmd.Run = c.Run
 	return c
 }
@@ -48,18 +54,23 @@ func (b *ManifestCmd) Run(cmd *cobra.Command, args []string) {
 	i18n.Err(err, i18n.ERR_INVALID_PACKAGE_NAME)
 	// get the package manifest
 	m := local.GetManifest(name)
-	// marshal the manifest
-	bytes, err := json.MarshalIndent(m, "", "  ")
-	core.CheckErr(err, "cannot marshal manifest")
-	// if no filter is set then return the whole manifest
-	if len(b.filter) == 0 {
-		fmt.Printf("%v\n", string(bytes))
-	} else {
-		var jason interface{}
-		err := json.Unmarshal(bytes, &jason)
-		// otherwise apply the jsonpath to extract a value from the manifest
-		result, err := jsonpath.Read(jason, b.filter)
-		core.CheckErr(err, "cannot apply filter expression '%s'", b.filter)
-		fmt.Printf("%v", result)
+	if b.format == "json" {
+		// marshal the manifest
+		bytes, err := json.MarshalIndent(m, "", "  ")
+		core.CheckErr(err, "cannot marshal manifest")
+		// if no filter is set then return the whole manifest
+		if len(b.filter) == 0 {
+			fmt.Printf("%v\n", string(bytes))
+		} else {
+			var jason interface{}
+			err := json.Unmarshal(bytes, &jason)
+			// otherwise apply the jsonpath to extract a value from the manifest
+			result, err := jsonpath.Read(jason, b.filter)
+			core.CheckErr(err, "cannot apply filter expression '%s'", b.filter)
+			fmt.Printf("%v", result)
+		}
+	} else if b.format == "mdf" {
+		bytes := m.ToMarkDownBytes(name.String())
+		ioutil.WriteFile(path.Join(core.WorkDir(), "manifest.md"), bytes, os.ModePerm)
 	}
 }
