15635c13596a5b2ee5b30e44947a30232e1812c3
gatblau <onix@gatblau.org>
2021-08-14 16:17:53 +0100

    remove duplicate notice

--- piloth/syslog/server.go
+++ piloth/syslog/server.go
@@ -19,14 +19,6 @@ import (
 	"time"
 )
 
-/*
-  Onix Config Manager - Pilot
-  Copyright (c) 2018-2021 by www.gatblau.org
-  Licensed under the Apache License, Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0
-  Contributors to this project, hereby assign copyright in this code to the project,
-  to be licensed under the same terms as the rest of the code.
-*/
-
 type Server struct {
 	bindIP string
 	port   string
