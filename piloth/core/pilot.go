d999715b0926cd49035b8820b24a23d5db5033cd
gatblau <onix@gatblau.org>
2021-08-28 11:01:10 +0100

    standarsise logging

--- piloth/core/pilot.go
+++ piloth/core/pilot.go
@@ -10,7 +10,6 @@ package core
 import (
 	"fmt"
 	"github.com/gatblau/onix/piloth/job"
-	"log"
 	"log/syslog"
 	"os"
 	"time"
@@ -53,7 +52,6 @@ func NewPilot() (*Pilot, error) {
 		cfg:    cfg,
 		info:   info,
 		ctl:    r,
-		logs:   logsWriter,
 		worker: worker,
 	}
 	// return a new pilot
@@ -70,88 +68,74 @@ func (p *Pilot) Start() {
 |  _/        _/_/_/  _/_/_/_/    _/_/        _/           |
 |                     Host Controller                     | 
 +---------------------------------------------------------+`)
-	log.Printf("launching...\n")
+	InfoLogger.Printf("launching...\n")
 	collector, err := NewCollector("0.0.0.0", p.cfg.getSyslogPort())
 	if err != nil {
-		p.stdout("cannot create pilot syslog collector: %s\n", err)
+		ErrorLogger.Printf("cannot create pilot syslog collector: %s\n", err)
 		os.Exit(1)
 	}
 	collector.Start()
 	if !commandExists("art") {
-		p.stdout("cannot find artisan CLI, ensure it is installed before running pilot\n")
+		ErrorLogger.Printf("cannot find artisan CLI, ensure it is installed before running pilot\n")
 		os.Exit(127)
 	}
 	p.register()
 	p.ping()
 }
 
-// warn: write a warning in syslog
-func (p *Pilot) warn(msg string, args ...interface{}) {
-	log.SetOutput(p.logs)
-	p.logs.Warning(fmt.Sprintf(msg+"\n", args...))
-}
-
-// stdout: write a message to stdout
-func (p *Pilot) stdout(msg string, args ...interface{}) {
-	log.SetOutput(os.Stdout)
-	log.Printf(msg+"\n", args...)
-}
-
 // register the host, keep retrying indefinitely until a registration is successful
 func (p *Pilot) register() {
 	// checks if the host is already registered
 	if !IsRegistered() {
-		p.stdout("host not registered, attempting registration")
+		InfoLogger.Printf("host not registered, attempting registration\n")
 		// starts a loop
 		for {
 			err := p.ctl.Register()
 			// if no error then exit the loop
 			if err == nil {
-				p.stdout("registration successful")
+				InfoLogger.Printf("registration successful\n")
 				err = SetRegistered()
 				p.connected = true
 				if err != nil {
-					p.stdout("failed to cache registration status: %s", err)
+					ErrorLogger.Printf("failed to cache registration status: %s\n", err)
 				}
 				break
 			} else {
-				p.stdout("registration failed: %s", err)
+				ErrorLogger.Printf("registration failed: %s\n", err)
 				p.connected = false
 			}
 			// otherwise, waits for a period before retrying
-			p.stdout("waiting 60s before attempting registration again")
+			InfoLogger.Printf("waiting 60s before attempting registration again")
 			time.Sleep(1 * time.Minute)
 		}
 	} else {
-		p.stdout("host is already registered")
+		InfoLogger.Printf("host is already registered\n")
 	}
 }
 
 func (p *Pilot) ping() {
-	p.stdout("starting ping loop")
+	InfoLogger.Printf("starting ping loop\n")
 	for {
 		cmd, err := p.ctl.Ping()
 		if err != nil {
 			// write to the console output
-			p.stdout("ping failed: %s", err)
+			InfoLogger.Printf("ping failed: %s\n", err)
 			p.connected = false
 		} else {
 			if !p.connected {
-				p.stdout("ping loop operational")
+				InfoLogger.Printf("ping loop operational\n")
 			}
 			p.connected = true
 			// verify the host identity and command value integrity using Pretty Good Privacy
 			err = verify(cmd.Value, cmd.Signature)
 			// if the verification fails, it is likely spoofing of pilotctl has happened
 			if err != nil {
-				p.stdout("invalid host signature, cannot trust the pilot control service => %s", err)
-				// as it cannot trust the host writes a warning to syslog
-				p.warn("invalid host signature, cannot trust the pilot control service => %s", err)
+				WarningLogger.Printf("invalid host signature, cannot trust the pilot control service => %s\n", err)
 			} else { // if the host can be trusted
 				// do we have a command to process?
 				if cmd.Value.JobId > 0 {
 					// execute the job
-					p.stdout("starting execution of job #%v, package => '%s', fx => '%s'", cmd.Value.JobId, cmd.Value.Package, cmd.Value.Function)
+					InfoLogger.Printf("starting execution of job #%v, package => '%s', fx => '%s'\n", cmd.Value.JobId, cmd.Value.Package, cmd.Value.Function)
 					p.worker.AddJob(cmd.Value)
 				}
 			}
@@ -162,6 +146,6 @@ func (p *Pilot) ping() {
 
 func (p *Pilot) debug(msg string, a ...interface{}) {
 	if len(os.Getenv("PILOT_DEBUG")) > 0 {
-		p.stdout(fmt.Sprintf("DEBUG: %s", msg), a...)
+		DebugLogger.Printf(msg, a...)
 	}
 }
