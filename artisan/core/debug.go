b4111eb92828fc30fa30ac8d370c7cc4143fe6dd
gatblau <onix@gatblau.org>
2021-08-30 09:46:20 +0100

    issue stderr warning only if in debug mode

--- artisan/core/debug.go
+++ artisan/core/debug.go
@@ -14,12 +14,12 @@ import (
 
 // Debug writes a debug message to the console
 func Debug(msg string, a ...interface{}) {
-	if inDebugMode() {
+	if InDebugMode() {
 		DebugLogger.Printf("%s\n", fmt.Sprintf(msg, a...))
 	}
 }
 
 // check for a ARTISAN_DEBUG variable set
-func inDebugMode() bool {
+func InDebugMode() bool {
 	return len(os.Getenv("ARTISAN_DEBUG")) > 0
 }
