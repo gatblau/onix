efef795553643889deeeaabe7cd40c16c75b73a5
gatblau <onix@gatblau.org>
2021-09-06 13:04:19 +0100

    add build number to version at build time

---
+++ piloth/core/version.go
@@ -0,0 +1,12 @@
+package core
+
+/*
+  Onix Config Manager - Host Pilot
+  Copyright (c) 2018-2021 by www.gatblau.org
+  Licensed under the Apache License, Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0
+  Contributors to this project, hereby assign copyright in this code to the project,
+  to be licensed under the same terms as the rest of the code.
+*/
+
+// Version the version displayed on cli
+var Version = "0.0.4"
