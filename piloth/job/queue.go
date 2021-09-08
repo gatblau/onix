679b7364a82dd0f874244872305c065a42e22155
gatblau <onix@gatblau.org>
2021-09-08 15:39:23 +0100

    single source for JobResult type between pilot and pilotctl

---
+++ piloth/job/queue.go
@@ -0,0 +1,14 @@
+package job
+
+/*
+  Onix Config Manager - Pilot
+  Copyright (c) 2018-2021 by www.gatblau.org
+  Licensed under the Apache License, Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0
+  Contributors to this project, hereby assign copyright in this code to the project,
+  to be licensed under the same terms as the rest of the code.
+*/
+
+// Queue a persistent queue to manage communication between Pilot and Pilot Control
+// in a resilient manner
+type Queue struct {
+}
