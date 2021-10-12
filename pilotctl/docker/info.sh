d80f203ab97b79d4ca99359d220c24a57277d90f
Steve Cliff <steve@devcloud.guru>
2021-10-12 12:09:44 +0100

    autogenerate - first release now completed

--- pilotctl/docker/info.sh
+++ pilotctl/docker/info.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 
+OPTIONS='%-40s | %-40s | %-40s \n'
+
 . .env
 
 # Display summary for developer/tester
 echo "Technical Info"
 echo
-echo "Onix Swagger"
-echo "http://localhost:8080/swagger-ui.html"
-echo ${ONIX_HTTP_ADMIN_USER}:${ONIX_HTTP_ADMIN_PWD}
-echo
-echo "Pilotctl Swagger"
-echo "http://localhost:8888/api/index.html"
-echo ${PILOTCTL_ONIX_USER}:${PILOTCTL_ONIX_PWD}
-echo
-echo "Event Receiver (Mongo) Swagger"
-echo "http://localhost:${PILOTCTL_EVR_MONGO_PORT}/api/index.html"
-echo ${PILOTCTL_EVR_MONGO_UNAME}:${PILOTCTL_EVR_MONGO_PWD}
-echo
-echo "Remote Artisan Registry Links"
-echo "http://artreg.apsedge.io/api/index.html"
-echo "http://nexus.apsedge.io"
-echo
-echo "Local Artisan Registry Links"
-echo "http://${ART_REG_URI}:${ART_REG_PORT}"
-echo ${ART_REG_USER}:${ART_REG_PWD}
+
+printf "$OPTIONS" "------------" "------------" "------------"
+printf "$OPTIONS" "Service" "URI" "Credentials"
+printf "$OPTIONS" "------------" "------------" "------------"
+printf "$OPTIONS" "Onix Swagger" "http://localhost:8080/swagger-ui.html" "${ONIX_HTTP_ADMIN_USER}:${ONIX_HTTP_ADMIN_PWD}"
+printf "$OPTIONS" "Pilotctl Swagger" "http://localhost:8888/api/index.html" "${PILOTCTL_ONIX_USER}:${PILOTCTL_ONIX_PWD}"
+printf "$OPTIONS" "Event Receiver (Mongo) Swagger" "http://localhost:${PILOTCTL_EVR_MONGO_PORT}/api/index.html" "${PILOTCTL_EVR_MONGO_UNAME}:${PILOTCTL_EVR_MONGO_PWD}"
+printf "$OPTIONS" "Local Artisan Registry Links" "http://${ART_REG_URI}:${ART_REG_PORT}" "${ART_REG_USER}:${ART_REG_PWD}"
+printf "$OPTIONS" "PilotCtl database (direct Postgres)" "http://localhost:5432" "${PG_ADMIN_USER}:${PG_ADMIN_PWD}"
+printf "$OPTIONS" "Pilotctl database (Web GUI)" "http://localhost:8083" "admin@local.com:${PG_ADMIN_PWD}"
+printf "$OPTIONS" "------------" "------------" "------------"
+printf "$OPTIONS" "Demo Artisan Registry" "http://artreg.apsedge.io/api/index.html" "Please contact for credentials"
+printf "$OPTIONS" "------------" "------------" "------------"
+
 echo
 echo "Current Container tags"
-echo CIT_OX_APP -------- $( echo ${CIT_OX_APP}  | awk -F":" '{print $2}' )
-echo CIT_PILOTCTL_APP -- $( echo ${CIT_PILOTCTL_APP}  | awk -F":" '{print $2}' )
-echo CIT_ARTREG_APP ---- $( echo ${CIT_ARTREG_APP}  | awk -F":" '{print $2}' )
-echo CIT_DBMAN --------- $( echo ${CIT_DBMAN}  | awk -F":" '{print $2}' )
-echo CIT_EVRMONGO_APP -- $( echo ${CIT_EVRMONGO_APP}  | awk -F":" '{print $2}' )
+cat .env | grep  --color=never CIT_ | sort
