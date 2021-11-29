94c8fee070b31ffd24460bffee98d45e89fd9102
gatblau <onix@gatblau.org>
2021-11-29 12:04:02 +0000

    pwd and name generators

---
+++ artisan/deploy/rndname_test.go
@@ -0,0 +1,25 @@
+/*
+  Onix Config Manager - Artisan
+  Copyright (c) 2018-2021 by www.gatblau.org
+  Licensed under the Apache License, Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0
+  Contributors to this project, hereby assign copyright in this code to the project,
+  to be licensed under the same terms as the rest of the code.
+*/
+
+package deploy
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestName(t *testing.T) {
+	// names with numbers
+	for i := 0; i < 20; i++ {
+		fmt.Println(RandomName(99))
+	}
+	// names with no numbers
+	for i := 0; i < 20; i++ {
+		fmt.Println(RandomName(0))
+	}
+}
