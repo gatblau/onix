a55f8366184d47e3bcddf29ff71039d3629b562e
gatblau <onix@gatblau.org>
2021-12-01 13:21:18 +0000

    introduce app level vars and svc dependency calculation

--- artisan/deploy/app.go
+++ artisan/deploy/app.go
@@ -22,6 +22,7 @@ type AppManifest struct {
 	Description string   `yaml:"description,omitempty"`
 	Version     string   `yaml:"version"`
 	Services    []SvcRef `yaml:"services"`
+	Var         Vars     `yaml:"var,omitempty"`
 }
 
 type SvcRef struct {
@@ -41,6 +42,8 @@ type SvcRef struct {
 	Port string `yaml:"port,omitempty"`
 	// the service manifest loaded from remote image
 	Info *SvcManifest `yaml:"service,omitempty"`
+	// the other services it depends on
+	DependsOn []string `yaml:"depends_on,omitempty"`
 }
 
 // NewAppMan creates a new application manifest from an URI (supported schemes are http(s):// and file://
@@ -94,22 +97,115 @@ func (m *AppManifest) Wire() (*AppManifest, error) {
 			if strings.HasPrefix(strings.Replace(v.Value, " ", "", -1), "{{fx=") {
 				content := v.Value[len("{{fx=") : len(v.Value)-2]
 				parts := strings.Split(content, ":")
+				// qualifies the name of the variable with the service name
+				vName := fmt.Sprintf("${%s_%s}", strings.ToUpper(strings.Replace(service.Name, "-", "_", -1)), v.Name)
 				switch strings.ToLower(parts[0]) {
 				case "pwd":
 					subParts := strings.Split(parts[1], ",")
 					length, _ := strconv.Atoi(subParts[0])
 					symbols, _ := strconv.ParseBool(subParts[1])
-					appMan.Services[six].Info.Var[vix].Value = RandomPwd(length, symbols)
+					appMan.Services[six].Info.Var[vix].Value = vName
+					appMan.Var.Items = append(appMan.Var.Items, AppVar{
+						Name:        vName,
+						Description: v.Description,
+						Value:       RandomPwd(length, symbols),
+						Secret:      true,
+						Service:     strings.ToUpper(service.Name),
+					})
 				case "name":
 					number, _ := strconv.Atoi(parts[1])
-					appMan.Services[six].Info.Var[vix].Value = RandomName(number)
+					appMan.Services[six].Info.Var[vix].Value = vName
+					appMan.Var.Items = append(appMan.Var.Items, AppVar{
+						Name:        vName,
+						Description: v.Description,
+						Value:       RandomName(number),
+						Secret:      false,
+						Service:     strings.ToUpper(service.Name),
+					})
 				default:
-
+					return nil, fmt.Errorf("invalid function %s='%s' in service '%s'\n", v.Name, v.Value, service.Name)
+				}
+			} else // if the variable is a binding
+			if strings.HasPrefix(strings.Replace(v.Value, " ", "", -1), "{{bind=") {
+				content := v.Value[len("{{bind=") : len(v.Value)-2]
+				parts := strings.Split(content, ":")
+				switch len(parts) {
+				case 1:
+					svcName := parts[0]
+					// check the name exists
+					found := false
+					for _, s := range m.Services {
+						if s.Name == svcName {
+							found = true
+							break
+						}
+					}
+					if !found {
+						return nil, fmt.Errorf("invalid service name '%s' => %s='%s' in service '%s'\n", svcName, v.Name, v.Value, service.Name)
+					}
+					appMan.Services[six].Info.Var[vix].Value = svcName
+				case 2:
+					switch parts[1] {
+					case "schema_uri":
+						if uri := m.getSchemaURI(parts[0]); len(uri) > 0 {
+							appMan.Services[six].Info.Var[vix].Value = uri
+						} else {
+							return nil, fmt.Errorf("variable %s='%s' in service '%s' request schema_ui from service '%s' but is missing\n", v.Name, v.Value, service.Name, parts[0])
+						}
+					}
+				default:
+					return nil, fmt.Errorf("invalid binding %s='%s' in service '%s'\n", v.Name, v.Value, service.Name)
+				case 3:
+					switch parts[1] {
+					case "var":
+						if m.varExists(parts[2]) {
+							appMan.Services[six].Info.Var[vix].Value = strings.ToUpper(fmt.Sprintf("${%s_%s}", parts[0], parts[2]))
+							appMan.Services[six].DependsOn = addDependency(appMan.Services[six].DependsOn, parts[0])
+						} else {
+							return nil, fmt.Errorf("cannot find variable %s='%s' in service '%s'\n", v.Name, v.Value, service.Name)
+						}
+					}
 				}
 			}
 		}
 	}
 	return appMan, nil
+}
+
+func addDependency(dependsOn []string, svc string) []string {
+	result := make([]string, len(dependsOn))
+	copy(result, dependsOn)
+	exists := false
+	for _, d := range result {
+		if d == svc {
+			exists = true
+			break
+		}
+	}
+	if !exists {
+		result = append(result, svc)
+	}
+	return result
+}
+
+func (m *AppManifest) getSchemaURI(svc string) string {
+	for _, service := range m.Services {
+		if service.Name == svc && len(service.SchemaURI) > 0 {
+			return service.SchemaURI
+		}
+	}
+	return ""
+}
+
+func (m *AppManifest) varExists(varName string) bool {
+	for _, service := range m.Services {
+		for _, v := range service.Info.Var {
+			if v.Name == varName {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func (m *AppManifest) validate() error {
