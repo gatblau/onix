6fc408c83235665e523db80d0e86dd0ce5f6e3fb
gatblau <onix@gatblau.org>
2021-12-14 14:23:12 +0000

    add ascii text art

--- dbman/cmd/rootCmd.go
+++ dbman/cmd/rootCmd.go
@@ -1,8 +1,9 @@
 //   Onix Config DatabaseProvider - Dbman
-//   Copyright (c) 2018-2020 by www.gatblau.org
+//   Copyright (c) 2018-2021 by www.gatblau.org
 //   Licensed under the Apache License, Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0
 //   Contributors to this project, hereby assign copyright in this code to the project,
 //   to be licensed under the same terms as the rest of the code.
+
 package cmd
 
 import (
@@ -16,14 +17,23 @@ type RootCmd struct {
 	*cobra.Command
 }
 
+// https://textkool.com/en/ascii-art-generator?hl=default&vl=default&font=Broadway%20KB&text=dbman%0A
+
 func NewRootCmd() *RootCmd {
 	c := &RootCmd{
 		&cobra.Command{
 			Use:   "dbman",
 			Short: "database manager",
-			Long: `dbman is a CLI tool to manage database schema released versions, upgrade data and perform database backups and restores.
-	dbman is part of (and used by) Onix Configuration DatabaseProvider (see https://onix.gatblau.org) to manage its configuration database.
-	dbman can also be run from a container (when in http mode) to manage the data / schema life cycle of databases from a container platform.`,
+			Long: `
++++++++++++++++++| ONIX CONFIG MANAGER |+++++++++++++++++
+|              ___   ___   _       __    _              |
+|             | | \ | |_) | |\/|  / /\  | |\ |          |
+|             |_|_/ |_|_) |_|  | /_/--\ |_| \|          |
+|                  Manage Database Schemas              |
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+dbman is a CLI tool to manage database schema versions and upgrades.
+dbman is part of (and used by) Onix Configuration DatabaseProvider (see https://onix.gatblau.org) to manage its configuration database.
+dbman can also be run from a container (when in http mode) to manage the data / schema life cycle of databases from a container platform.`,
 		},
 	}
 	cobra.OnInitialize(c.initConfig)
