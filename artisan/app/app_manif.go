8fab494659bbb5e33d0347db84280ae0ffe4c8f5
gatblau <onix@gatblau.org>
2021-12-26 19:52:59 +0000

    rename behaviours in app manifest

--- artisan/app/app_manif.go
+++ artisan/app/app_manif.go
@@ -92,7 +92,7 @@ type SvcRef struct {
 	// the other services using it
 	UsedBy []string `yaml:"used_by_count,omitempty"`
 	// instructions to customise deployment
-	Behave map[string]string `yaml:"behave,omitempty"`
+	Is map[string]string `yaml:"is,omitempty"`
 }
 
 // NewAppMan creates a new application manifest from an URI (supported schemes are http(s):// and file://
