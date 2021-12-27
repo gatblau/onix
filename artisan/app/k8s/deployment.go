e054b7b84585e15c87f300d2ef71d29264440c0c
gatblau <onix@gatblau.org>
2021-12-27 12:07:45 +0000

    support job marshalling

--- artisan/app/k8s/deployment.go
+++ artisan/app/k8s/deployment.go
@@ -92,11 +92,13 @@ type Template struct {
 }
 
 type Spec struct {
-	Replicas int      `yaml:"replicas,omitempty"`
-	Selector Selector `yaml:"selector,omitempty"`
-	Template Template `yaml:"template,omitempty"`
-	Ports    []Ports  `yaml:"ports,omitempty"`
-	Type     string   `yaml:"type,omitempty"`
-	TLS      []TLS    `yaml:"tls,omitempty"`
-	Rules    []Rules  `yaml:"rules,omitempty"`
+	Replicas    int      `yaml:"replicas,omitempty"`
+	Selector    Selector `yaml:"selector,omitempty"`
+	Template    Template `yaml:"template,omitempty"`
+	Ports       []Ports  `yaml:"ports,omitempty"`
+	Type        string   `yaml:"type,omitempty"`
+	TLS         []TLS    `yaml:"tls,omitempty"`
+	Rules       []Rules  `yaml:"rules,omitempty"`
+	Parallelism int      `yaml:"parallelism,omitempty"`
+	Completions int      `yaml:"completions,omitempty"`
 }
