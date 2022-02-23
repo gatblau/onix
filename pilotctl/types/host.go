497a0d17028755b55aa5b9625f98218a7f07a0b3
gatblau <onix@gatblau.org>
2022-02-23 11:07:13 +0000

    add mac address to host query

--- pilotctl/types/host.go
+++ pilotctl/types/host.go
@@ -1,23 +1,24 @@
-package types
-
 /*
   Onix Config Manager - Pilot Control
-  Copyright (c) 2018-2021 by www.gatblau.org
+  Copyright (c) 2018-Present by www.gatblau.org
   Licensed under the Apache License, Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0
   Contributors to this project, hereby assign copyright in this code to the project,
   to be licensed under the same terms as the rest of the code.
 */
 
+package types
+
 // Host monitoring information
 type Host struct {
-	HostUUID  string   `json:"host_uuid"`
-	OrgGroup  string   `json:"org_group"`
-	Org       string   `json:"org"`
-	Area      string   `json:"area"`
-	Location  string   `json:"location"`
-	Connected bool     `json:"connected"`
-	LastSeen  int64    `json:"last_seen"`
-	Since     int      `json:"since"`
-	SinceType string   `json:"since_type"`
-	Label     []string `json:"label"`
+	HostUUID       string   `json:"host_uuid"`
+	HostMacAddress string   `json:"host_mac_address"`
+	OrgGroup       string   `json:"org_group"`
+	Org            string   `json:"org"`
+	Area           string   `json:"area"`
+	Location       string   `json:"location"`
+	Connected      bool     `json:"connected"`
+	LastSeen       int64    `json:"last_seen"`
+	Since          int      `json:"since"`
+	SinceType      string   `json:"since_type"`
+	Label          []string `json:"label"`
 }
