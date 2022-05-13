9de673283073cda748d6a344068e0ed5f4d76bd9
gatblau <onix@gatblau.org>
2022-05-13 10:51:30 +0100

    clean up binaries with root owner after tarring

--- artisan/art/owner.sh
+++ artisan/art/owner.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
+# changes the ownership to root before tarring
 sudo chown root bin/darwin/amd64/art
 sudo chown root bin/darwin/arm64/art
 sudo chown root bin/linux/amd64/art
+# tarring binaries
 tar -zcvf art_linux_amd64.tar.gz -C bin/linux/amd64 .
 tar -zcvf art_darwin_amd64.tar.gz -C bin/darwin/amd64 .
 tar -zcvf art_darwin_arm64.tar.gz -C bin/darwin/arm64 .
+# removing binaries with root owner
+rm -r bin
