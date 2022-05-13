1e1220fa8a67910e33eba9cec3865da6ff6e04a5
gatblau <onix@gatblau.org>
2022-05-13 13:31:33 +0100

    improve build script

--- artisan/art/build.sh
+++ artisan/art/build.sh
@@ -1,11 +1,18 @@
-#!/bin/zsh
+#!/bin/bash
+# removing binaries with root owner
+echo removing previous binaries...
+rm -rf bin
+rm -f *.tar.gz
+# build all binaries
+echo building binaries...
+art run build-all
 # changes the ownership to root before tarring
+echo changing binary ownership...
 sudo chown root bin/darwin/amd64/art
 sudo chown root bin/darwin/arm64/art
 sudo chown root bin/linux/amd64/art
 # tarring binaries
+echo tarring binaries...
 tar -zcvf art_linux_amd64.tar.gz -C bin/linux/amd64 .
 tar -zcvf art_darwin_amd64.tar.gz -C bin/darwin/amd64 .
 tar -zcvf art_darwin_arm64.tar.gz -C bin/darwin/arm64 .
-# removing binaries with root owner
-rm -r bin
