014a2434448e9f05973080556ec7c6ff2d0d6838
gatblau <onix@gatblau.org>
2022-05-25 09:58:55 +0100

    use smtp_user instead of email_from to log into the smtp server

--- artisan/doorman/proxy/conf.go
+++ artisan/doorman/proxy/conf.go
@@ -9,78 +9,87 @@
 package main
 
 import (
-	"fmt"
-	"github.com/joho/godotenv"
-	"os"
-	"strconv"
+    "fmt"
+    "github.com/joho/godotenv"
+    "os"
+    "strconv"
 )
 
 func init() {
-	// load env vars from file if present
-	godotenv.Load("proxy.env")
+    // load env vars from file if present
+    godotenv.Load("proxy.env")
 }
 
 func isLoggingEnabled() bool {
-	value := os.Getenv("DPROXY_LOGGING")
-	return len(value) > 0
+    value := os.Getenv("DPROXY_LOGGING")
+    return len(value) > 0
 }
 
 func getEmailFrom() (string, error) {
-	value := os.Getenv("DPROXY_EMAIL_FROM")
-	if len(value) == 0 {
-		return "", fmt.Errorf("variable DPROXY_EMAIL_FROM is required and not defined")
-	}
-	return value, nil
+    value := os.Getenv("DPROXY_EMAIL_FROM")
+    if len(value) == 0 {
+        return "", fmt.Errorf("variable DPROXY_EMAIL_FROM is required and not defined")
+    }
+    return value, nil
 }
 
-func getEmailFromPwd() (string, error) {
-	value := os.Getenv("DPROXY_EMAIL_FROM_PWD")
-	if len(value) == 0 {
-		return "", fmt.Errorf("variable DPROXY_EMAIL_FROM_PWD is required and not defined")
-	}
-	return value, nil
+func getSmtpUser() (string, error) {
+    value := os.Getenv("DPROXY_SMTP_USER")
+    if len(value) == 0 {
+        fmt.Printf("WARNING: DPROXY_SMTP_USER not defined, using DPROXY_EMAIL_FROM instead\n")
+        return getEmailFrom()
+    }
+    return value, nil
+}
+
+func getSmtpPwd() (string, error) {
+    value := os.Getenv("DPROXY_SMTP_PWD")
+    if len(value) == 0 {
+        return "", fmt.Errorf("variable DPROXY_SMTP_PWD is required and not defined")
+    }
+    return value, nil
 }
 
 func getSmtpHost() (string, error) {
-	value := os.Getenv("DPROXY_SMTP_HOST")
-	if len(value) == 0 {
-		return "", fmt.Errorf("variable DPROXY_SMTP_HOST is required and not defined")
-	}
-	return value, nil
+    value := os.Getenv("DPROXY_SMTP_HOST")
+    if len(value) == 0 {
+        return "", fmt.Errorf("variable DPROXY_SMTP_HOST is required and not defined")
+    }
+    return value, nil
 }
 
 func getSmtpPort() (int, error) {
-	value := os.Getenv("DPROXY_SMTP_PORT")
-	if len(value) == 0 {
-		return -1, fmt.Errorf("variable DPROXY_SMTP_PORT is required and not defined")
-	}
-	port, err := strconv.Atoi(value)
-	if err != nil {
-		return -1, fmt.Errorf("invalid DPROXY_SMTP_PORT value: %s; %s", value, err)
-	}
-	return port, nil
+    value := os.Getenv("DPROXY_SMTP_PORT")
+    if len(value) == 0 {
+        return -1, fmt.Errorf("variable DPROXY_SMTP_PORT is required and not defined")
+    }
+    port, err := strconv.Atoi(value)
+    if err != nil {
+        return -1, fmt.Errorf("DPROXY_SMTP_PORT value should be numeric but its value was found to be '%s'; %s", value, err)
+    }
+    return port, nil
 }
 
 func getDoormanBaseURI() (string, error) {
-	value := os.Getenv("DPROXY_DOORMAN_URI")
-	if len(value) == 0 {
-		return "", fmt.Errorf("variable DPROXY_DOORMAN_URI is required and not defined")
-	}
-	return value, nil
+    value := os.Getenv("DPROXY_DOORMAN_URI")
+    if len(value) == 0 {
+        return "", fmt.Errorf("variable DPROXY_DOORMAN_URI is required and not defined")
+    }
+    return value, nil
 }
 
 func getDoormanUser() (string, error) {
-	value := os.Getenv("DPROXY_DOORMAN_USER")
-	if len(value) == 0 {
-		return "", fmt.Errorf("variable DPROXY_DOORMAN_USER is required and not defined")
-	}
-	return value, nil
+    value := os.Getenv("DPROXY_DOORMAN_USER")
+    if len(value) == 0 {
+        return "", fmt.Errorf("variable DPROXY_DOORMAN_USER is required and not defined")
+    }
+    return value, nil
 }
 
 func getDoormanPwd() (string, error) {
-	value := os.Getenv("DPROXY_DOORMAN_PWD")
-	if len(value) == 0 {
-		return "", fmt.Errorf("variable DPROXY_DOORMAN_PWD is required and not defined")
-	}
-	return value, nil
+    value := os.Getenv("DPROXY_DOORMAN_PWD")
+    if len(value) == 0 {
+        return "", fmt.Errorf("variable DPROXY_DOORMAN_PWD is required and not defined")
+    }
+    return value, nil
 }
