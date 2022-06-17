50ed99ed049d8faa2a94f0254c87b7cd223247c3
gatblau <onix@gatblau.org>
2022-06-17 13:25:42 +0100

    ability to talk to pilotctl via http proxy

--- piloth/core/lib.go
+++ piloth/core/lib.go
@@ -9,51 +9,53 @@
 package core
 
 import (
-	"encoding/base64"
-	"fmt"
-	"log"
-	"math"
-	"os"
-	"os/exec"
-	"os/user"
-	"path/filepath"
-	"time"
+    "encoding/base64"
+    "fmt"
+    "log"
+    "math"
+    "net/http"
+    "net/url"
+    "os"
+    "os/exec"
+    "os/user"
+    "path/filepath"
+    "time"
 )
 
 // HomeDir pilot's home directory
 func HomeDir() string {
-	defer TRA(CE())
-	usr, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return usr.HomeDir
+    defer TRA(CE())
+    usr, err := user.Current()
+    if err != nil {
+        log.Fatal(err)
+    }
+    return usr.HomeDir
 }
 
 // reverse the passed-in string
 func reverse(str string) (result string) {
-	defer TRA(CE())
-	for _, v := range str {
-		result = string(v) + result
-	}
-	return
+    defer TRA(CE())
+    for _, v := range str {
+        result = string(v) + result
+    }
+    return
 }
 
 func newToken(hostUUID, hostIP, hostName string) string {
-	defer TRA(CE())
-	// create an authentication token as follows:
-	// 1. takes host uuid (i.e. machine Id + hostname hash), host ip, name and unix time
-	// 2. base 64 encode
-	// 3. reverse string
-	return reverse(
-		base64.StdEncoding.EncodeToString(
-			[]byte(fmt.Sprintf("%s|%s|%s|%d", hostUUID, hostIP, hostName, time.Now().Unix()))))
+    defer TRA(CE())
+    // create an authentication token as follows:
+    // 1. takes host uuid (i.e. machine Id + hostname hash), host ip, name and unix time
+    // 2. base 64 encode
+    // 3. reverse string
+    return reverse(
+        base64.StdEncoding.EncodeToString(
+            []byte(fmt.Sprintf("%s|%s|%s|%d", hostUUID, hostIP, hostName, time.Now().Unix()))))
 }
 
 func commandExists(cmd string) bool {
-	defer TRA(CE())
-	_, err := exec.LookPath(cmd)
-	return err == nil
+    defer TRA(CE())
+    _, err := exec.LookPath(cmd)
+    return err == nil
 }
 
 // nextInterval calculates the next retry interval using exponential backoff strategy
@@ -63,35 +65,53 @@ func commandExists(cmd string) bool {
 //   - n is the number of failures that have occurred
 //   - multiplier is an arbitrary multiplier that can be replaced with any suitable value
 func nextInterval(failureCount float64) time.Duration {
-	defer TRA(CE())
-	// multiplier 2.0 yields 15s, 60s, 135s, 240s, 375s, 540s, etc
-	interval := 15 * math.Pow(2.0, failureCount)
-	// puts a maximum limit of 1 hour
-	if interval > 3600 {
-		interval = 3600
-	}
-	duration, err := time.ParseDuration(fmt.Sprintf("%fs", interval))
-	if err != nil {
-		ErrorLogger.Printf(err.Error())
-	}
-	return duration
+    defer TRA(CE())
+    // multiplier 2.0 yields 15s, 60s, 135s, 240s, 375s, 540s, etc
+    interval := 15 * math.Pow(2.0, failureCount)
+    // puts a maximum limit of 1 hour
+    if interval > 3600 {
+        interval = 3600
+    }
+    duration, err := time.ParseDuration(fmt.Sprintf("%fs", interval))
+    if err != nil {
+        ErrorLogger.Printf(err.Error())
+    }
+    return duration
 }
 
 func (p *Pilot) debug(msg string, a ...interface{}) {
-	if len(os.Getenv("PILOT_DEBUG")) > 0 {
-		DebugLogger.Printf(msg, a...)
-	}
+    if len(os.Getenv("PILOT_DEBUG")) > 0 {
+        DebugLogger.Printf(msg, a...)
+    }
 }
 
 func Abs(path string) string {
-	defer TRA(CE())
-	if !filepath.IsAbs(path) {
-		p, err := filepath.Abs(path)
-		if err != nil {
-			fmt.Printf("cannot work out absolute path for %s: %s\n", path, err)
-			os.Exit(1)
-		}
-		path = p
-	}
-	return path
+    defer TRA(CE())
+    if !filepath.IsAbs(path) {
+        p, err := filepath.Abs(path)
+        if err != nil {
+            fmt.Printf("cannot work out absolute path for %s: %s\n", path, err)
+            os.Exit(1)
+        }
+        path = p
+    }
+    return path
+}
+
+// Proxy returns a proxy func or nil if PILOT_HTTP_PROXY is not set
+// Nil value means no proxy used in http.Transport
+func Proxy() func(*http.Request) (*url.URL, error) {
+    // constructs a proxy func if a proxy URL has been defined
+    var proxy func(*http.Request) (*url.URL, error)
+    // if a proxy url is set
+    if v := os.Getenv("PILOT_HTTP_PROXY"); len(v) > 0 {
+        proxyURL, err := url.Parse(v)
+        if err != nil {
+            log.Printf("invalid proxy URL: %s, proxy will be disabled\n", err)
+        } else {
+            // set the value of proxy function
+            proxy = http.ProxyURL(proxyURL)
+        }
+    }
+    return proxy
 }
