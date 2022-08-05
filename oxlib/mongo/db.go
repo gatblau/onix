c0181faeb72fcc93f5374133ed02762cc5126fdb
gatblau <onix@gatblau.org>
2022-08-05 20:09:54 +0100

    update comments

--- oxlib/mongo/db.go
+++ oxlib/mongo/db.go
@@ -1,5 +1,5 @@
 /*
-  Onix Config Manager - Artisan's Doorman
+  Onix Config Manager - Mongo Library
   Copyright (c) 2018-Present by www.gatblau.org
   Licensed under the Apache License, Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0
   Contributors to this project, hereby assign copyright in this code to the project,
