0289676d2dfe286d756e66f30fdd1423afe92ef7
Ubuntu <ubuntu@ip-10-0-0-60.us-west-2.compute.internal>
2022-08-29 06:58:45 +0000

    fixedsome of the copy paste error. Modified build.yaml file to generate md doc. Added version.go file

---
+++ telemetry/collector/version.go
@@ -0,0 +1,13 @@
+/*
+  Onix Config Manager - OpenTelemetry collector for managed hosts
+  Copyright (c) 2018-2022 by www.gatblau.org
+  Licensed under the Apache License, Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0
+  Contributors to this project, hereby assign copyright in this code to the project,
+  to be licensed under the same terms as the rest of the code.
+*/
+
+package collector
+
+// Version placeholder variable for the version number displayed on cli
+// this value gets overridden at build time with the correct version in the build file
+var Version = "0.0.0.0"
