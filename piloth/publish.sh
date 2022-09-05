77ae59f63f72b3d12ac8ccfd5726d40786e3525f
gatblau <onix@gatblau.org>
2022-09-05 15:00:39 +0100

    tidy files

--- piloth/publish.sh
+++
@@ -1,2 +0,0 @@
-art build -t artisanregistry-nexus-sonar-ansible.apps.amosocp.amosonline.io/gatblau/piloth
-art push -u admin:admin123 artisanregistry-nexus-sonar-ansible.apps.amosocp.amosonline.io/gatblau/piloth
